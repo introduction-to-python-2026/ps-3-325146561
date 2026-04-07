78add4bed89093fecd9627cff5d02b74ea00d158
325146561 <noasela2002@gmail.com>
2026-04-07 12:21:30 +0300

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
   
     for n in range(n_terms):
-        pi_approx += (-1)**n / (2*n + 1)
+        pi_approx = (-1)**n / (2*n + 1)
     pi_approx *= 4
     return pi_approx
